982978ec8e3b81cbc1db32cc50dd2a2d483fe96f
hhiroki <gyouzakuuka@gmail.com>
2021-09-11 18:13:44 +0900

    TODOの追加ができるところまで

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use std::str::FromStr;
 
 struct Model {
     link: ComponentLink<Self>,
@@ -8,6 +9,9 @@ struct Model {
 
 struct State {
     tasks: Vec<Task>,
+    new_task_name: String,
+    new_task_assignee: String,
+    new_task_estimate: u32,
 }
 
 impl State {
@@ -16,6 +20,9 @@ impl State {
     }
     fn decrease_status(&mut self, idx: usize) {
         self.tasks.get_mut(idx).filter(|e| e.status > 1).map(|e| e.status = e.status - 1);
+    }
+    fn add_new_task(&mut self, name: String, assignee: String, estimate: u32) {
+        self.tasks.push(Task { name, assignee, estimate, status: 1 });
     }
 }
 
@@ -29,6 +36,10 @@ struct Task {
 enum Msg {
     IncreaseStatus(usize),
     DecreaseStatus(usize),
+    UpdateNewTaskName(String),
+    UpdateNewTaskAssignee(yew::html::ChangeData),
+    UpdateNewTaskEstimate(String),
+    NewTask,
 }
 
 impl Component for Model {
@@ -43,7 +54,10 @@ impl Component for Model {
                     Task { name: "Task 2".to_string(), assignee: "🐶".to_string(), estimate: 2, status: 1 },
                     Task { name: "Task 3".to_string(), assignee: "🐱".to_string(), estimate: 1, status: 2 },
                     Task { name: "Task 4".to_string(), assignee: "🐹".to_string(), estimate: 3, status: 3 },
-                ]
+                ],
+                new_task_name: "".to_string(),
+                new_task_assignee: "🐱".to_string(),
+                new_task_estimate: 0,
             }
         }
     }
@@ -52,6 +66,21 @@ impl Component for Model {
         match msg {
             Msg::IncreaseStatus(idx) => self.state.increase_status(idx),
             Msg::DecreaseStatus(idx) => self.state.decrease_status(idx),
+            Msg::UpdateNewTaskName(value) => self.state.new_task_name = value,
+            Msg::UpdateNewTaskAssignee(value) => {
+                if let yew::html::ChangeData::Select(v) = value {
+                    self.state.new_task_assignee = v.value();
+                }
+            },
+            Msg::UpdateNewTaskEstimate(value) => {
+                if let Ok(v) = u32::from_str(&value) {
+                    self.state.new_task_estimate = v;
+                }
+            },
+            Msg::NewTask => {
+                // TODO: add validate
+                self.state.add_new_task(self.state.new_task_name.clone(), self.state.new_task_assignee.clone(), self.state.new_task_estimate);
+            }
         }
         true
     }
@@ -77,6 +106,12 @@ impl Component for Model {
                     </div>
                 </section>
 
+                <section class="section">
+                    <div class="container">
+                        { self.view_header() }
+                    </div>
+                </section>
+
                 <section id="board" class="section">
                     <div class="container">
                         <div class="columns">
@@ -92,6 +127,33 @@ impl Component for Model {
 }
 
 impl Model {
+    fn view_header(&self) -> Html {
+        html! {
+            <div class="container">
+                <input
+                    type="text"
+                    value={self.state.new_task_name.clone()}
+                    oninput={self.link.callback(|e: InputData| Msg::UpdateNewTaskName(e.value))}
+                />
+
+                <select onchange={self.link.callback(|e: ChangeData| Msg::UpdateNewTaskAssignee(e))}>
+                    <option value="🐱">{ "🐱" }</option>
+                    <option value="🐶">{ "🐶" }</option>
+                    <option value="🐹">{ "🐹" }</option>
+                </select>
+
+                <input
+                    type="text"
+                    value={self.state.new_task_estimate.clone().to_string()}
+                    oninput={self.link.callback(|e: InputData| Msg::UpdateNewTaskEstimate(e.value))}
+                />
+                <button onclick={self.link.callback(|_| Msg::NewTask)}>
+                    { "追加" }
+                </button>
+            </div>
+        }
+    }
+
     fn view_column(&self, status: u32, status_text: &str, tasks: &Vec<Task>) -> Html {
         html! {
             <div class=format!("column status-{}", status)>
