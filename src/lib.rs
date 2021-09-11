57ea9a3f8029964fb114ebda81bb8da3ffba2383
hhiroki <gyouzakuuka@gmail.com>
2021-09-11 16:52:11 +0900

    タスクカードを移動できるようになるまで

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,21 @@ use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
 struct Model {
+    link: ComponentLink<Self>,
     state: State,
 }
 
 struct State {
     tasks: Vec<Task>,
+}
+
+impl State {
+    fn increase_status(&mut self, idx: usize) {
+        self.tasks.get_mut(idx).filter(|e| e.status < 3).map(|e| e.status = e.status + 1);
+    }
+    fn decrease_status(&mut self, idx: usize) {
+        self.tasks.get_mut(idx).filter(|e| e.status > 1).map(|e| e.status = e.status - 1);
+    }
 }
 
 struct Task {
@@ -17,13 +27,16 @@ struct Task {
 }
 
 enum Msg {
+    IncreaseStatus(usize),
+    DecreaseStatus(usize),
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Model {
+            link,
             state: State {
                 tasks: vec! [
                     Task { name: "Task 1".to_string(), assignee: "🐱".to_string(), estimate: 3, status: 1 },
@@ -35,8 +48,12 @@ impl Component for Model {
         }
     }
 
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        false
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::IncreaseStatus(idx) => self.state.increase_status(idx),
+            Msg::DecreaseStatus(idx) => self.state.decrease_status(idx),
+        }
+        true
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -63,9 +80,9 @@ impl Component for Model {
                 <section id="board" class="section">
                     <div class="container">
                         <div class="columns">
-                            { view_column(1, "未対応", &self.state.tasks) }
-                            { view_column(2, "処理中", &self.state.tasks) }
-                            { view_column(3, "完了"  , &self.state.tasks) }
+                            { self.view_column(1, "未対応", &self.state.tasks) }
+                            { self.view_column(2, "処理中", &self.state.tasks) }
+                            { self.view_column(3, "完了"  , &self.state.tasks) }
                         </div>
                     </div>
                 </section>
@@ -74,34 +91,37 @@ impl Component for Model {
     }
 }
 
-fn view_column(status: u32, status_text: &str, tasks: &Vec<Task>) -> Html {
-    html! {
-        <div class=format!("column status-{}", status)>
-            <div class="tags has-addons">
-                <span class="tag">{ status_text }</span> <span class="tag is-dark">{ tasks.iter().filter(|e| e.status == status).count() }</span>
+impl Model {
+    fn view_column(&self, status: u32, status_text: &str, tasks: &Vec<Task>) -> Html {
+        html! {
+            <div class=format!("column status-{}", status)>
+                <div class="tags has-addons">
+                    <span class="tag">{ status_text }</span> <span class="tag is-dark">{ tasks.iter().filter(|e| e.status == status).count() }</span>
+                </div>
+                { for tasks.iter().enumerate().filter(|e| e.1.status == status).map(|e| self.view_task(e)) }
             </div>
-            { for tasks.iter().enumerate().filter(|e| e.1.status == status).map(view_task) }
-        </div>
+        }
+    }
+
+    fn view_task(&self, (idx, task): (usize, &Task)) -> Html {
+        html! {
+            <div class="card">
+                <div class="card-content">
+                    { &task.name }
+                </div>
+                <footer class="card-footer">
+                    <div class="card-footer-item">{ &task.assignee }</div>
+                    <div class="card-footer-item">{ format!("{} 人日", &task.estimate) }</div>
+                </footer>
+                <footer class="card-footer">
+                    <a class="card-footer-item" onclick={self.link.callback(move |_| Msg::DecreaseStatus(idx))}>{ "◀︎︎" }</a>
+                    <a class="card-footer-item" onclick={self.link.callback(move |_| Msg::IncreaseStatus(idx))}>{ "▶︎︎" }</a>
+                </footer>
+            </div>
+        }
     }
 }
 
-fn view_task((_idx, task): (usize, &Task)) -> Html {
-    html! {
-        <div class="card">
-            <div class="card-content">
-                { &task.name }
-            </div>
-            <footer class="card-footer">
-                <div class="card-footer-item">{ &task.assignee }</div>
-                <div class="card-footer-item">{ format!("{} 人日", &task.estimate) }</div>
-            </footer>
-            <footer class="card-footer">
-                <a class="card-footer-item">{ "◀︎" }</a>
-                <a class="card-footer-item">{ "▶︎︎" }</a>
-            </footer>
-        </div>
-    }
-}
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
